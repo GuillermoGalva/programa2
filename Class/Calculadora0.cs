3f22068097a81ec219de599aef4c0a78a4db6acd
guill <guill@GUILLERMO-PC>
2024-05-02 21:00:12 -0400

    Add project files.

---
+++ Class/Calculadora0.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace programa2.Class
+{
+    using System;
+
+    class Calculadora0
+    {
+        public static int CalcularSuma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int CalcularProducto(int num3, int num4)
+        {
+            return num3 * num4;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using programa2.Class;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, num3, num4;
+
+        try
+        {
+
+            Console.WriteLine("Ingrese el primer número:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer número:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el cuarto número:");
+            num4 = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Por favor ingrese un número entero válido.");
+            return;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: El número ingresado es demasiado grande.");
+            return;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return;
+        }
+
+
+        int suma = Calculadora0.CalcularSuma(num1, num2);
+
+        
+        int producto = Calculadora0.CalcularProducto(num3, num4);
+
+      
+        Console.WriteLine($"La suma de los dos primeros números es: {suma}");
+        Console.WriteLine($"El producto del tercer y cuarto número es: {producto}");
+    }
+}
